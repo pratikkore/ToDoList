ed1c8781c8c72d2769f33101c93218ae7bf7c802
pratikkore <pratikkore5051@gmail.com>
2023-02-02 22:12:46 +0530

    add

---
+++ src/component/Todoinput.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+function Todoinput(props) {
+  const [inputText, setInputText] = useState("");
+  return (
+    <div className="input-container">
+      <input
+        type="text"
+        className="input-box-todo"
+        placeholder="Enter Your To Do......"
+        value={inputText}
+        onChange={(e) => {
+          setInputText(e.target.value);
+        }}
+      />
+      <button
+        className="add-btn"
+        onClick={() => {
+          props.addList(inputText);
+          setInputText("");
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+export default Todoinput;
